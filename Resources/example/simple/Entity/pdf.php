9d15b3fb0ad5703816a326a54ec66e712db0e6c9
chadyred <chadyred@cellierflorian.com>
2015-05-30 14:07:14 +0200

    Update pdf.php

--- Resources/example/simple/Entity/pdf.php
+++ Resources/example/simple/Entity/pdf.php
@@ -4,34 +4,32 @@ namespace Acme\DemoBundle\Entity;
 
 class Pdf extends \FPDI
 {
-
 	/**
-	* Write test
+	* Write text at position XY in a rectangle with (width, height, $string)
 	*/
 	function destinataireDenomination($string)
 	{ 
-
-		// Décalage de 10 cm vers la droite et 40 vers le bas
+		
 		$this->SetXY(102, 47);
 		$this->Cell(200,10,$string,0,0,'l',0); 
 	} 
-
+	
 	/**
-	* Write test
+	* Write text at position XY in a rectangle with (width, height, $string)
 	*/
 	function destinataireComplement($string)
 	{ 
-		// Décalage de 8 cm à droite
+		
 		$this->SetXY(102, 52);
 		$this->Cell(200,10,$string,0,0,'l',0); 
 	} 
-
+	
 	/**
 	* Checkbox to check
 	*/
 	function checkboxDemandeDt()
 	{
-
+	
 		$this->Line(11.3, 55.8, 13, 58.5, $style);
 		$this->Line(13, 58.5, 15, 54.5, $style);
 	}
@@ -44,28 +42,27 @@ class Pdf extends \FPDI
 	    
 	    return 'uploads/pdf';
 	}
-
     
-    /**
-    * relatif path to upload dir
-    */
-    public function getUploadRootDir()
-    {
-        
-        // On retourne le chemin relatif vers l'image pour notre code PHP
-        return __DIR__.'/../../../../web/'.$this->getUploadDir();
-    }
-
-
-    /**
-    * webPath
-    *
-    * Récuperation du fichier
-    * 
-    * @return chemin de notre fichier de manière relative
-    */
-    public function getWebPath()
-    {
-        return $this->getUploadDir() . '/my_pdf.pdf';
-    }
+	/**
+	* relatif path to upload dir
+	*/
+	public function getUploadRootDir()
+	{
+	
+	// On retourne le chemin relatif vers l'image pour notre code PHP
+	return __DIR__.'/../../../../web/'.$this->getUploadDir();
+	}
+	
+	
+	/**
+	* webPath
+	*
+	* Récuperation du fichier
+	* 
+	* @return chemin de notre fichier de manière relative
+	*/
+	public function getWebPath()
+	{
+	return $this->getUploadDir() . '/my_pdf.pdf';
+	}
 }
