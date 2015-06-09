c2f9571efaf253dcd36839bd48f316d6682cb799
Florian CELLIER <vivedrummer@hotm.fr>
2015-06-09 11:16:09 +0200

    test decorator

--- lib/cellfit/cellfit.php
+++ lib/cellfit/cellfit.php
@@ -10,8 +10,9 @@ class FPDF_CellFit extends \DecoratorPdf {
 
 		//Calculate ratio to fit cell
 		if($w==0)
-			$w = $this->w-$this->rMargin-$this->x;
-		$ratio = ($w-$this->cMargin*2)/$str_width;
+			$w = $this->pdf->w-$this->pdf->rMargin-$this->x;
+
+		$ratio = ($w-$this->pdf->cMargin*2)/$str_width;
 
 		$fit = ($ratio < 1 || ($ratio > 1 && $force));
 		if ($fit)
@@ -21,25 +22,25 @@ class FPDF_CellFit extends \DecoratorPdf {
 				//Calculate horizontal scaling
 				$horiz_scale=$ratio*100.0;
 				//Set horizontal scaling
-				$this->_out(sprintf('BT %.2F Tz ET',$horiz_scale));
+				$this->pdf->_out(sprintf('BT %.2F Tz ET',$horiz_scale));
 			}
 			else
 			{
 				//Calculate character spacing in points
-				$char_space=($w-$this->cMargin*2-$str_width)/max($this->MBGetStringLength($txt)-1,1)*$this->k;
+				$char_space=($w-$this->pdf->cMargin*2-$str_width)/max($this->MBGetStringLength($txt)-1,1)*$this->pdf->k;
 				//Set character spacing
-				$this->_out(sprintf('BT %.2F Tc ET',$char_space));
+				$this->pdf->_out(sprintf('BT %.2F Tc ET',$char_space));
 			}
 			//Override user alignment (since text will fill up cell)
 			$align='';
 		}
 
 		//Pass on to Cell method
-		$this->Cell($w,$h,$txt,$border,$ln,$align,$fill,$link);
+		$this->pdf->Cell($w,$h,$txt,$border,$ln,$align,$fill,$link);
 
 		//Reset character spacing/horizontal scaling
 		if ($fit)
-			$this->_out('BT '.($scale ? '100 Tz' : '0 Tc').' ET');
+			$this->pdf->_out('BT '.($scale ? '100 Tz' : '0 Tc').' ET');
 	}
 
 	//Cell with horizontal scaling only if necessary
