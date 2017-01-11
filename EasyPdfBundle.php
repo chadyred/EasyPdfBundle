a86db2a72f331a5d78bdce4ed0ee698161359174
f_cellier <fcellier@norsys.fr>
2017-01-11 10:04:10 +0100

    Better doc

---
+++ EasyPdfBundle.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * @author:  Florian CELLIER <chadyred@gmail.com> 
+ * @license: MI
+ *
+ */
+namespace Kark\EasyPdfBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EasyPdfBundle extends Bundle
+{}
--- Lib/DecoratorPdf.php
+++ Lib/DecoratorPdf.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace lib;
+namespace Kark\EasyPdfBundle\Lib;
 
 abstract class DecoratorPdf extends \FPDF {
     protected $pdf;
  
-    public function __construct (\FPDF $pdf=NULL, $orientation='P', $unit='mm', $size='A4') {
+    public function __construct (\FPDF $pdf, $orientation='P', $unit='mm', $size='A4') {
     	parent::__construct ($orientation='P', $unit='mm', $size='A4');
     	$this->pdf = $pdf;
     }
